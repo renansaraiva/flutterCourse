d0b1208248cdf74d8c693f200127af5c06607254
renansaraiva <renan.saraiva@gmail.com>
2025-04-20 19:56:13 -0300

    Seção 2 - Aula 34 - Usando Column para Organizar os Widgets - Troquei o RaisedButton por ElevatedButton

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +7,34 @@ void main() {
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null
+            )
+          ],
+        ),
       )
     );
   }
