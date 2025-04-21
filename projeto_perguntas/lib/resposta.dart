c25ff302a569c17cfafd5d35f68298bcce0b8fda
renansaraiva <renan.saraiva@gmail.com>
2025-04-21 17:19:08 -0300

    Seção 2 - Aula 48 - Passando Função Callback

--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      child: Text(texto)
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: quandoSelecionado,
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue
+          ),
+          child: Text(texto)
+      )
     );
   }
 }
