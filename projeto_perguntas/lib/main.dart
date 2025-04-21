2ab1983e71a5fa3ee484a8050f5827d57f5ab360
renansaraiva <renan.saraiva@gmail.com>
2025-04-21 17:28:39 -0300

    Seção 2 - Aula 50 - Usando Tipo Map nas Perguntas - precisei adicionar o List<Map> em perguntas para funcionar

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -8,46 +8,50 @@ void main() {
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-  
+
   void _responder() {
     setState(() {
       _perguntaSelecionada++;
     });
-    print(_perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final List<String> perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?'
+    final List<Map> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Pedro']
+      }
     ];
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
-          ],
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Perguntas'),
+      ),
+      body: Column(
+        children: [
+          Questao(perguntas[_perguntaSelecionada]['texto']),
+          Resposta('Resposta 1', _responder),
+          Resposta('Resposta 2', _responder),
+          Resposta('Resposta 3', _responder),
+        ],
+      ),
+    ));
   }
-
 }
 
 class PerguntaApp extends StatefulWidget {
-  
   @override
   _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
-
 }
